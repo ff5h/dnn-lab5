255a9358d37c7c6b22c73bfab4bb99e5340932ac
rybkin <vdj.manka@gmail.com>
2026-05-20 02:03:10 +0300

    usecase-2 lstm

--- src/train.py
+++ src/train.py
@@ -1,74 +1,108 @@
 import wandb
 from wandb.integration.keras import WandbMetricsLogger, WandbModelCheckpoint
 
-import random
-import numpy as np
 import tensorflow as tf
+
+import os
+os.makedirs("models/usecase-2: LSTM/checkpoints", exist_ok=True)
+
+max_words = 5000
+(X_train, y_train), (X_test, y_test) = tf.keras.datasets.imdb.load_data(num_words=max_words)
+
+max_len = 500
+X_train = tf.keras.preprocessing.sequence.pad_sequences(X_train, maxlen=max_len)
+X_test = tf.keras.preprocessing.sequence.pad_sequences(X_test, maxlen=max_len)
 
 wandb.init(
     project="dnn_lab5",
     config={
-        # conv block 1
-        "conv1_filters": 32,
-        "conv1_kernel_size": 5,
-        "conv1_activation": "relu",
-        # conv block 2
-        "conv2_filters": 64,
-        "conv2_kernel_size": 5,
-        "conv2_activation": "relu",
-        # dense layers
-        "dense_1": 1000,
-        "dense_2": 500,
-        "dense_3": 250,
-        "dense_activation": "relu",
-        "output_units": 10,
-        # regularization
-        "dropout": 0.5,
-        # training
+        "max_len": max_len,
+        "max_words": max_words,
+        "embedding_dim": 32,
+        "lstm_units": 100,
+        "batch_size": 64,
+        "epochs": 5,
         "optimizer": "adam",
-        "loss": "categorical_crossentropy",
-        "from_logits": True,
-        "metric": "accuracy",
-        "epoch": 10,
-        "batch_size": 256
+        "loss": "binary_crossentropy",
     }
 )
 
 config = wandb.config
 
-cifar10 = tf.keras.datasets.cifar10
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
-y_train = tf.keras.utils.to_categorical(y_train)
-y_test = tf.keras.utils.to_categorical(y_test)
+class RNN(tf.keras.Model):
+    def __init__(
+        self,
+        max_len: int = config.max_len,
+        max_words: int = config.max_words,
+        embedding_dim: int = config.embedding_dim,
+        lstm_units: int = config.lstm_units,
+        **kwargs,
+    ):
+        super(RNN, self).__init__(**kwargs)
 
-model = tf.keras.models.Sequential([
-    tf.keras.Input(shape=(32, 32, 3)),
-    tf.keras.layers.Conv2D(filters=config.conv1_filters, kernel_size=(config.conv1_kernel_size, config.conv1_kernel_size), activation=config.conv1_activation),
-    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-    tf.keras.layers.Conv2D(filters=config.conv2_filters, kernel_size=(config.conv2_kernel_size, config.conv2_kernel_size), activation=config.conv2_activation),
-    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(config.dense_1, activation=config.dense_activation),
-    tf.keras.layers.Dropout(rate=config.dropout),
-    tf.keras.layers.Dense(config.dense_2, activation=config.dense_activation),
-    tf.keras.layers.Dropout(rate=config.dropout),
-    tf.keras.layers.Dense(config.dense_3, activation=config.dense_activation),
-    tf.keras.layers.Dropout(rate=config.dropout),
-    tf.keras.layers.Dense(config.output_units)
-])
+        self.max_len = max_len
+        self.max_words = max_words
+        self.embedding_dim = embedding_dim
+        self.lstm_units = lstm_units
 
-model.compile(optimizer=config.optimizer,
-              loss=tf.keras.losses.CategoricalCrossentropy(from_logits=config.from_logits),
-              metrics=[config.metric])
+        self.network = tf.keras.Sequential([
+            tf.keras.layers.Input(shape=(max_len,), name="inputs"),
+            tf.keras.layers.Embedding(input_dim=max_words, output_dim=embedding_dim, input_length=max_len, name="embedding"),
+            tf.keras.layers.LSTM(units=lstm_units, name="lstm"),
+            tf.keras.layers.Dense(units=1, name="output"),
+            tf.keras.layers.Activation("sigmoid", name="sigmoid"),
+        ], name="rnn_sequential")
 
-history = model.fit(x=x_train, y=y_train,
-                    epochs=config.epoch,
-                    batch_size=config.batch_size,
-                    validation_split=0.2,
-                    callbacks=[
-                      WandbMetricsLogger(log_freq=5),
-                      WandbModelCheckpoint("models/model.keras")
-                    ])
+    def call(self, x):
+        return self.network(x)
+
+    def get_config(self):
+        config = super(RNN, self).get_config()
+        config.update({
+            "max_len": self.max_len,
+            "max_words": self.max_words,
+            "embedding_dim": self.embedding_dim,
+            "lstm_units": self.lstm_units,
+        })
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
+
+model = RNN()
+model.compile(
+    loss=config.loss,
+    optimizer=config.optimizer,
+    metrics=["accuracy"],
+)
+
+print(model.network.summary())
+
+callbacks = [
+    tf.keras.callbacks.EarlyStopping(
+        monitor="val_loss",
+        min_delta=0.0001,
+        patience=5,
+        restore_best_weights=True,
+    ),
+    WandbMetricsLogger(log_freq="epoch"),
+    WandbModelCheckpoint(
+        filepath="models/usecase-2: LSTM/checkpoints/rnn_{epoch:02d}.keras",
+        monitor="val_loss"
+    ),
+]
+
+history = model.fit(
+    X_train,
+    y_train,
+    batch_size=config.batch_size,
+    epochs=config.epochs,
+    validation_split=0.2,
+    callbacks=callbacks,
+)
+
+scores = model.evaluate(X_test, y_test)
+print("Accuracy: %.2f%%" % (scores[1]*100))
 
 wandb.finish()
